495e201c2eed9d8de2e8ddcb9e01f3ad4fe40dc4
eu-manuel <100456791+eu-manuel@users.noreply.github.com>
2025-06-18 18:36:26 -0300

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '123456789ç',
+  password: '',
   database: 'projeto_db'
 });
 
